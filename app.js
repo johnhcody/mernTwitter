c198497436c0105f0ce6d34eb1dd2b7b5dfc5fac
johnhcody <john.haner.cody@gmail.com>
2020-10-04 13:21:20 +0700

    /login route complete

--- app.js
+++ app.js
@@ -2,14 +2,36 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
+app.use(bodyParser.json());
+
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello All"));
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: 'john',
+        email: 'john@john.com',
+        password: 'password'
+    })
+    user.save()
+    console.log(res);
+    res.send("Hello All")
+});
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
