80f939642c9c3204d2598f7fa22c4680bd161d53
johnhcody <john.haner.cody@gmail.com>
2020-10-04 21:08:03 +0700

    tweet backend routes set up

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const User = require('./models/User');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 app.use(bodyParser.urlencoded({ 
     extended: false 
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,31 @@ const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+
+// You may want to start commenting in information about your routes so that you can find the appropriate ones quickly.
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
+})
+
+
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
     // Check to make sure nobody has already registered with a duplicate email
 
     User.findOne({ email: req.body.email })
@@ -46,12 +67,28 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: 'Success' });
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            }
+                        )
                     } else {
-                        return res.status(400).json({ password: "Incorrect" })
+                        return res.status(400).json({ password: "Incorrect password" })
                     }
                 }) 
         })
 })
+
 
 module.exports = router;
